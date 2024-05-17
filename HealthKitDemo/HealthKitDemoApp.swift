0ef3496263c9340e4812f7b68753bf18452b21a1
Mariusz <mariusz@berriesinthebasket.com>
2024-05-17 13:18:18 +0200

    Initial Commit

---
+++ HealthKitDemo/HealthKitDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HealthKitDemoApp.swift
+//  HealthKitDemo
+//
+//  Created by Mariusz Smoliński on 17.05.24.
+//
+
+import SwiftUI
+
+@main
+struct HealthKitDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
