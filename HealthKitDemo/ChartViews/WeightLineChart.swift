14262d6f65e4ec1f96baa668b52884a9de03ed1c
Mariusz <mariusz@berriesinthebasket.com>
2024-07-25 22:59:25 +0200

    adds styling to the weight line chart

--- HealthKitDemo/ChartViews/WeightLineChart.swift
+++ HealthKitDemo/ChartViews/WeightLineChart.swift
@@ -12,6 +12,10 @@ struct WeightLineChart: View {
     var selectedStat: HealthMetric
     var chartData: [HealthData]
     
+    var minValue: Double{
+        chartData.map{ $0.value }.min() ?? 0
+    }
+    
     var body: some View {
         VStack{
             NavigationLink(value: selectedStat){
@@ -31,16 +35,35 @@ struct WeightLineChart: View {
             .foregroundStyle(.secondary)
             
             Chart{
+                RuleMark(y: .value("Weight Goal", 155))
+                    .foregroundStyle(.indigo)
+                    .lineStyle(.init(lineWidth: 1, dash: [5]))
+                
                 ForEach(chartData){weight in
-                    AreaMark(x: .value("Day", weight.date, unit: .day), y: .value("Value", weight.value))
+                    AreaMark(x: .value("Day", weight.date, unit: .day), yStart: .value("Value", weight.value), yEnd: .value("Min Value", minValue))
                         .foregroundStyle(Gradient(colors: [.green.opacity(0.5), .clear]))
+                        .interpolationMethod(.catmullRom)
                     
                     LineMark(x: .value("Day", weight.date, unit: .day), y: .value("Value", weight.value))
                         .foregroundStyle(Color.green)
+                        .interpolationMethod(.catmullRom)
+                        .symbol(.circle)
                 }
             }
             .frame(height: 150)
-            
+            .chartYScale(domain: .automatic(includesZero: false))
+            .chartXAxis{
+                AxisMarks{
+                    AxisValueLabel(format: .dateTime.day().month(.defaultDigits))
+                }
+            }
+            .chartYAxis {
+                AxisMarks{value in
+                    AxisGridLine()
+                        .foregroundStyle(Color.secondary.opacity(0.3))
+                    AxisValueLabel()
+                }
+            }
         }
         .padding()
         .background(RoundedRectangle(cornerRadius: 12).fill(Color.gray.opacity(0.2)))
