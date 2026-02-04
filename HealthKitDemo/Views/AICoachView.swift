9c317c1c5b461d1dd5fd09585cc4d9d36ab1e651
Mariusz <mariusz@berriesinthebasket.com>
2026-02-04 23:19:15 +0100

    adds basic AI Coach View

---
+++ HealthKitDemo/Views/AICoachView.swift
@@ -0,0 +1,18 @@
+//
+//  AICoachView.swift
+//  HealthKitDemo
+//
+//  Created by Mariusz Smoliński on 04.02.26.
+//
+
+import SwiftUI
+
+struct AICoachView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AICoachView()
+}
